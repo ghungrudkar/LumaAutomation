aafc5bfb0067224854da9c736694041007de6a0f
Anuja ghungrudkar <anuja.ghungrudkar@gmail.com>
2023-08-09 14:19:02 +0530

    update

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser firefox
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 
 rem pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser firefox
 rem pytest -s -v -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
